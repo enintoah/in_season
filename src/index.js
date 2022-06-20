1daeb3e5900f54aed87ad599aeb25e9ae437880a
Anthonie Lorsithong <anthoniecorps@gmail.com>
2022-06-20 09:10:28 -0700

    W9D5

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-// import 'bootstrap'; 
-// import "./style.scss"
+const { default: Util } = require("./scripts/util");
+const { default: Vegetable } = require("./scripts/vegetable");
+
+window.Util = Util;
+window.Vegetable = Vegetable;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log("Dom fully loaded!!!")
+  const home = document.getElementById("homepage")
+  const spring = document.getElementById("spring")
+  const summer = document.getElementById("summer")
+  const autumn = document.getElementById("autumn")
+  const winter = document.getElementById("winter")
+})
